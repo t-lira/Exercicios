4c2fa7bf6662e604d0233c2bdf921691f65ca8eb
Thais Lira <lirapolinario@gmail.com>
2024-04-12 00:04:02 -0300

    resposta do exercicio 03

--- 01-introducao/main.c
+++ 01-introducao/main.c
@@ -55,3 +55,42 @@ int main() {
 
   return 0;
 }
+
+/*
+3) A jornada de trabalho semanal de um funcionário é de 40 horas. O funcionário que trabalhar mais
+de 40 horas receberá hora extra, cujo cálculo é o valor da hora regular com um acréscimo de 50%.
+Escreva um algoritmo que leia o número de horas trabalhadas em um mês, o salário por hora e escreva
+o salário total do funcionário, que deverá ser acrescido das horas extras, caso tenham sido trabalhadas (considere que o mês possua 4 semanas exatas). 
+*/
+
+#include <stdio.h>
+
+int main(void) {
+    int horas_mensais, horas_extras;
+    float salario_hora, salario_total, valor_hora_extra;
+
+    // Leitura das informações
+    printf("Digite o número de horas trabalhadas no mês: ");
+    scanf("%d", &horas_mensais);
+
+    printf("Digite o valor do salário por hora: ");
+    scanf("%f", &salario_hora);
+
+  // Cálculo do salário 
+   salario_total = horas_mensais * salario_hora; 
+
+  if (horas_mensais >= 40) {
+
+    valor_hora_extra = salario_total * 1.5;
+
+    // Exibição do salário total com acrescimo de 50%
+    printf("O salário total do funcionário é: %.2f\n", salario_total);
+    printf("O salário total do funcionário com as horas extras: %.2f\n", valor_hora_extra);
+    
+  } else{
+    // Exibição do salário total 
+    printf("O salário total do funcionário é: %.2f\n", salario_total);
+  }
+
+    return 0;
+}
